0e25989f4d7c426c562f9edaea61f7e8c5e441c9
Zeya Firdaus Widyaka <zwidyaka91@gmail.com>
2025-08-02 13:00:49 +0700

    adding some step to check playwright funtionality

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,31 +1,32 @@
-// @ts-check
 import { test, expect } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("https://duckduckgo.com/");
+  await page.goto("https://playwright.dev/");
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/DuckDuckGo/);
-
-  // //Type on the google search bar
-  // const searchBox = page.getByRole("combobox", { name: /Cari|Search/i });
-  // await searchBox.waitFor({ state: "visible", timeout: 10000 });
-  // await page.getByRole("button", { name: "Tetap logout" }).click();
-  // await searchBox.fill("Arema FC");
-  // await page.waitForTimeout(500);
-  // await searchBox.press("Enter");
-  // await locator.check('iframe[name="a-h9z35se4mx8s"]').contentFrame().getByRole("checkbox", { name: "I'm not a robot" });
-
-  // // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/Arema FC/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
-// test("get started link", async ({ page }) => {
-//   await page.goto("https://playwright.dev/");
+test("get started link", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
 
-//   // Click the get started link.
-//   await page.getByRole("link", { name: "Get started" }).click();
+  // Click the get started link.
+  await page.getByRole("link", { name: "Get started" }).click();
 
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
-// });
+  // Expects page to have a heading with the name of Installation.
+  await expect(page.getByRole("heading", { name: "Installation" })).toBeVisible();
+
+  // Click Writing test link.
+  await page.getByRole("link", { name: "Writing tests", exact: true }).click();
+
+  // Expect page to have a heading with the name of Writing tests.
+  await expect(page.getByRole("heading", { name: "Writing tests" })).toBeVisible();
+
+  // click search bar
+  await page.getByRole("button", { name: "Search" }).click();
+  await page.locator('input[type="search"]').fill("Test Runner");
+  await page.getByRole("button", { name: "Clear the query" }).click();
+
+  // await page.keyboard.press("Enter");
+  // await expect(page.getByRole("heading", { name: "Test Runner" })).toBeVisible();
+});
